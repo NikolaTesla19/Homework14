38f56d6a6182bbe0e9d83d098552003e9b72ce22
Denis <Denis.Efremov.ru@gmail.com>
2021-07-06 20:43:57 +0300

    production

--- index.js
+++ index.js
@@ -27,17 +27,57 @@ email: "jason@edabit.com"
 
 
 function comparison (obj1, obj2){
-  let  comparison= true;
-  if (obj1.name !== obj2.name) {
-    comparison = false;
+  let arr1 = [];
+  let arr2 = [];
+  for(let key in obj1){
+    arr1.push([key, obj1[key]]);
   }
-  if (obj1.phone !== obj2.phone) {
-    comparison = false;
+  for(let key in obj2){
+    arr2.push([key, obj2[key]]);
   }
-  if (obj1.name !== obj2.name) {
-    comparison = false;
+  if ((arr1.length - arr2.length) > 0) {
+    comparisonOfArr(arr1, arr2);
+  }else{
+    comparisonOfArr(arr2, arr1);
   }
-  console.log(comparison);
+}
+function comparisonOfArr(arr1, arr2){
+  let statusOfArr = true;
+  for (let i = 0; i < (arr1.length - arr2.length); i++) {
+    arr2.push([undefined, undefined]);
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    for (let n = 0; n < arr1[i].length; n++){
+      if (arr1[i][n] !== arr2[i][n]) {
+        statusOfArr = false;
+      }
+    }
+  }
 }
 comparison(objFirst1, objFirst2);
 comparison(objSecond1, objSecond2);
+//-----------------------------------------------2задание---------------------------------------------------------
+function expandedForm(number){
+  let arr = [];
+  let i = 1;
+  while ((number * 10) >= Math.pow(10, i)) {
+    arr.unshift(number % Math.pow(10, i));
+    number = number - (number % Math.pow(10, i));
+    i++;
+  }
+  let quantity = '';
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == 0) {
+      continue;
+    }
+    if (i == (arr.length - 1)) {
+      quantity =  quantity + arr[i];
+    }else {
+      quantity =  quantity + arr[i] + ' + ';
+    }
+  }
+  console.log(quantity);
+}
+expandedForm(70304);
+expandedForm(1037903);
+expandedForm(802539);
